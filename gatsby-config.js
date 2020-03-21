b6cb63d1317ce79a587fe92e40e95781d214d65f
Klajd Karaj <klajdkaraj14@gmail.com>
2020-03-21 22:40:27 +0000

    Add profile pic.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Financiest`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Financiest`,
+      summary: `Algorithmic, quantitative and financial concepts.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `Algorithmic, quantitative and financial concepts.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
@@ -66,7 +66,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/profile-pic.png`, 
       },
     },
     `gatsby-plugin-react-helmet`,
